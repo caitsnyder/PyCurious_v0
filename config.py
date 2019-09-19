5ca78cf6e20cff4f341bdf66b968c71dc035a423
Caitlin Snyder <caitsnyder@gmail.com>
2019-09-19 11:04:05 -0400

    With user registration

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-	SECRET_KEY = os.environ.get('SECRET_KEY	') or 'caitlin-secret-password'
+	SECRET_KEY = os.environ.get('SECRET_KEY	') or 'password-here-pre-deploy'
+
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+		'sqlite:///' + os.path.join(basedir,'app.db')
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
